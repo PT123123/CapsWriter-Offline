18ad4964acd93812317f3b3d8da35709f12387cf
PT123123 <31439216+PT123123@users.noreply.github.com>
2025-05-05 14:39:57 +0800

    Update build_exe_pyinstaller.bat
    解决unicode问题

--- build_exe_pyinstaller.bat
+++ build_exe_pyinstaller.bat
@@ -1,9 +1,10 @@
 @echo off
+chcp 65001
 REM 使用PyInstaller将tray_gui.py打包为exe
 REM 请确保已安装PyInstaller（pip install pyinstaller）
 REM 并在命令行中运行本脚本
 
-pyinstaller --noconfirm --onefile --windowed --icon=assets/icon.ico tray_gui.py
+pyinstaller --noconfirm --onefile --windowed --icon=assets/icon.ico --debug all tray_gui.py
 
 echo 打包完成！请在dist目录下查找生成的exe文件。
 pause
